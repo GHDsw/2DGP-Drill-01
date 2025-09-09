8e49320f3dab394798067a8e7d188d3a2cc45f55
GHDsw <hsw29371@gmail.com>
2025-09-09 11:59:47 +0900

    DRILL #1: 거북이를 "WASD"키로 조종하기
    과제 파일

---
+++ turtle_Lab_WASDmove.py
@@ -0,0 +1,28 @@
+import turtle
+import random
+
+def moveW():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+def moveA():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+def moveS():
+    turtle.setheading(-90)
+    turtle.forward(50)
+    turtle.stamp()
+def moveD():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+turtle.onkey(moveW,'w')
+turtle.onkey(moveA,'a')
+turtle.onkey(moveS,'s')
+turtle.onkey(moveD,'d')
+turtle.listen()
+
+turtle.mainloop()
+
